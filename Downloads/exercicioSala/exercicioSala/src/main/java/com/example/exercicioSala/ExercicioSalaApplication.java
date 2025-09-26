b18fa3d9b4c80092f00b9847bed5ab0767ce07f3
Pietra <ppietraa@hotmail.com>
2025-09-26 08:27:05 -0300

    Com JPA e H2

---
+++ Downloads/exercicioSala/exercicioSala/src/main/java/com/example/exercicioSala/ExercicioSalaApplication.java
@@ -0,0 +1,12 @@
+package com.example.exercicioSala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSalaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSalaApplication.class, args);
+	}
+
+}
